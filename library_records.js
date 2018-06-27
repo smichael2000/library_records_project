e62860824813b7eaefb15f932b5a7e438cc08822
Sheri Michael <smichael@tectonicgroup.com>
2018-06-27 12:07:06 -0600

    still need distinctAuthors

--- library_records.js
+++ library_records.js
@@ -5,7 +5,7 @@ var Library = function(){
 
 Library.prototype.addBook = function (book) {
   for (var i = 0; i < this._bookShelf.length; i++) {
-    if (this._bookShelf[i].title.indexOf(book.title)>-1){
+    if (this._bookShelf[i].title.indexOf(book)>-1){
       return false;
     }
   }
@@ -36,36 +36,52 @@ Library.prototype.getRandomBook = function () {
   var rand = this._bookShelf[Math.floor(Math.random() * this._bookShelf.length)];
   return rand;
 };
-  // return false;
 
+//This is not working properly
 Library.prototype.getBookByTitle = function (title) {
+  var booksbytitle = [];
   for(var i = 0; i<this._bookShelf.length; i++){
     if (this._bookShelf[i].title === title) {
-      return this._bookShelf[i];
+      console.log(this._bookShelf[i]);
+      booksbytitle.push(this._bookShelf[i]);
     }
-  return false;
+  }
+  if (booksbytitle.length == 0) {
+    return ("There are no books by that title");
+  }
+  else {
+    return booksbytitle;
   }
 };
 
 Library.prototype.getBooksByAuthor = function (authorName) {
+  var randAuthor = [];
   for(var i = 0; i<this._bookShelf.length; i++){
     if (this._bookShelf[i].author === authorName) {
-      return this._bookShelf[i];
+      randAuthor.push(this._bookShelf[i].title);
     }
   }
-  return false;
+  return ("The library has the following books by " + authorName +": " + randAuthor);
 };
 
 Library.prototype.addBooks = function (books) {
 
 };
 
-Library.prototype.getAuthors = function () {
-
+Library.prototype.getDistinctAuthors = function () {
+  var authors = this._bookShelf.author;
+    
+  // for(var i = 0; i<this._bookShelf.length; i++){
+  //   if(distinctAuthors.indexOf(this._bookShelf[i]) == -1){
+  //       distinctAuthors.push(this._bookShelf[i].author)
+  //   }
+  // }
+    return distinctAuthors;
 };
 
 Library.prototype.getRandomAuthorNames = function () {
-
+  var rand = this._bookShelf[Math.floor(Math.random() * this._bookShelf.length)];
+  return rand.author;
 };
 
 // Create book as object
@@ -76,11 +92,24 @@ var Book = function(title, author, numberOfPages, publishDate){
   this.publishDate= new Date(publishDate);
 }
 
+function booksInLibrary (){
+    gLibrary.addBook(book1)
+    gLibrary.addBook(book2)
+    gLibrary.addBook(book3)
+    gLibrary.addBook(book4)
+    gLibrary.addBook(book5)
+    gLibrary.addBook(book6)
+    gLibrary.addBook(book7)
+    return gLibrary;
+  }
+
 document.addEventListener("DOMContentLoaded", function() {
   window.gLibrary = new Library();
-  window.gbookOne = new Book ("IT", "Stephen King", 1138, "9-15-1986");
-  window.gbookTwo = new Book ("The Shining", "Stephen King", 447, "1-28-1977");
-  window.gbookThree = new Book ("The Big Four", "Agatha Christie", 282, "1-27-1927");
-  window.gbookFour = new Book ("A Thousand Acres", "Jane Smiley", 367, "10-23-1991");
-  window.gbookFive = new Book ("To Kill a Mockingbird", "Harper Lee", 281, "7-11-1960");
+  window.book1 = new Book ("IT", "Stephen King", 1138, "9-15-1986");
+  window.book2 = new Book ("The Shining", "Stephen King", 447, "1-28-1977");
+  window.book3 = new Book ("The Big Four", "Agatha Christie", 282, "1-27-1927");
+  window.book4 = new Book ("A Thousand Acres", "Jane Smiley", 367, "10-23-1991");
+  window.book5 = new Book ("To Kill a Mockingbird", "Harper Lee", 281, "7-11-1960");
+  window.book6 = new Book ("The Shining", "Petra", 501, "9-28-1997");
+  window.book7 = new Book ("Holden Reincarnated", "Collin Taylor", 104, "4-16-2001");
 });
