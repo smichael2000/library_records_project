fccaa80b9039e5005ee69970aab282facc4ca974
Sheri Michael <smichael@tectonicgroup.com>
2018-07-09 09:20:05 -0600

    Initial commit

---
+++ js/AddBooksUI.js
@@ -0,0 +1,22 @@
+var AddBooksUI = function(){
+this._tempBookShelf = [];
+this.$container = container;
+};
+
+AddBooksUI.prototype = Object.create(Library.prototype);
+
+AddBooksUI.prototype.init = function () {
+
+};
+
+AddBooksUI.prototype._bindevents = function () {
+  $('#add-books-btn').on('click', $.proxy(this.handleModalOpen, this));
+};
+
+AddBooksUI.prototype._handleModalOpen = funtion() {
+  this.$container.modal('show');
+}
+
+$(function(){
+window.fAddBooksUI= new AddBooksUI($('#addBookModal'));
+});
