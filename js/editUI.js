301dd66e0aba09c13728b5906a487a0d3599ada8
Sheri Michael <smichael@tectonicgroup.com>
2018-07-29 17:01:22 -0600

    edit ui almost done

--- js/editUI.js
+++ js/editUI.js
@@ -1,6 +1,7 @@
 var EditUI = function(){
   Library.call(this);
-  this.$container = $('editModal');
+  this.$container = $('#editModal');
+  this.title="";
   // console.log(this.$container);
 
 };
@@ -14,21 +15,31 @@ EditUI.prototype.init = function() {
   // window._bookShelf = this.retrieveBooks();
   // console.log(window._bookShelf);
   this._bindEvents();
+  // console.log("bound");
   return false;
 };
 
 EditUI.prototype._bindEvents = function() {
-  $('.editBtn').on('click', $.proxy(this._handleEdit, this));
-  $('.editBtn').on('click', this._openModal.bind(this))
+  $(document).on('click', '.editBtn', $.proxy(this._handleEdit, this));
+  $(document).on('click', '#saveChangesBtn', $.proxy(this._updateTable, this));
+  // $('.editBtn').on('click', this._openModal.bind(this))
   // console.log('_bindEvents');
 };
 
 EditUI.prototype._handleEdit = function(e) {
-  event.preventDefault();
-  // console.log("_handleSearch");
-  
-  this._handleEventTrigger('searchEvent', searchResult);
-
+  console.log(e);
+  console.log($(e.currentTarget).parent().parent());
+  //better to loop over and serach for 'title' in td
+  var td=$(e.currentTarget).parent().parent().children()[2];
+  this.title = $(td).data('title');
+  var book = this.getBookByTitle(title);
+  this.$container.find('#edit-title').attr('value',book[0].title);
+  this.$container.find('#edit-author').attr('value',book[0].author);
+  this.$container.find('#edit-pubDate').attr('value',book[0].pubDate);
+  this.$container.find('#edit-numPages').attr('value',book[0].numPages);
+  this.$container.find('#edit-genre').attr('value',book[0].genre);
+  // console.log(book[0], 'book');
+  this.$container.modal('show');
 };
 
 $(function(){
